877cbebd599d9f30e36e5e4d0a90ffb391973111
Daria Zaykina <=>
2019-06-18 20:26:27 +0300

    Almost got this

--- src/MultiThreading/Car.java
+++ src/MultiThreading/Car.java
@@ -1,9 +1,13 @@
 package MultiThreading;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by Levchenko Maksym on 13.06.2019.
@@ -27,67 +31,134 @@ class Car {
 }
 
 class RaceCarRunnable extends Car implements Runnable {
+    private static final double METERS_PER_SEC = 1000d / 3600;
+    private static final int ONE_SEC_MILLIS = 1000;
 
-    public RaceCarRunnable(String name, int maxSpeed, CountDownLatch countDownLatch, int passedDistance, int distance, boolean isFinished) {
+    public long getFinishTime() {
+        return FinishTime / ONE_SEC_MILLIS;
+    }
+
+    int FinishTime;
+
+    public RaceCarRunnable(String name, int maxSpeed, CountDownLatch countDownLatch, int passedDistance, int distance,
+                           boolean isFinished, int FinishTime) {
         super(name, maxSpeed);
         this.countDownLatch = countDownLatch;
         this.passedDistance = passedDistance;
         this.distance = distance;
         this.isFinished = isFinished;
+        this.FinishTime = FinishTime;
+
     }
 
-    private final CountDownLatch countDownLatch;
+    private CountDownLatch countDownLatch;
     int passedDistance;
     int distance;
     boolean isFinished;
 
+    public RaceCarRunnable(Car raceCar, int distance, CountDownLatch countDownLatch) {
+        super(raceCar.getName(), raceCar.getMaxSpeed());
+        this.distance = distance;
+        this.countDownLatch = countDownLatch;
+    }
+
+
     public int getRandomSpeed() {
 
 
-        return ThreadLocalRandom.current().nextInt(10, 50);
+        return ThreadLocalRandom.current().nextInt(getMaxSpeed() / 2, getMaxSpeed());
     }
 
     @Override
     public void run() {
 
-
         while (!isFinished) {
+            FinishTime = (int) (System.currentTimeMillis() - Race.startRaceTime.get());
             int currentSpeed = getRandomSpeed();
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("carName:" + getName() + "carSpeed:" + currentSpeed + "carProgress:" + passedDistance + " "
-                    + distance);
             passedDistance += (currentSpeed * 1000 / (60 * 60));
+
+
             if (passedDistance >= distance) {
                 isFinished = true;
-                System.out.println("FINISHED");
+                countDownLatch.countDown();
+                System.out.println(getName() + " " + "HAVE FINISHED");
             } else {
-                System.out.println(this);
+                System.out.println("CARNAME:" + " " + getName() + " " + "|CARSPEED:" + currentSpeed + " " + "|CARPROGRESS:" + " "
+                        + passedDistance + " " + distance+"\t"+"TIME"+" "+getFinishTime());
             }
         }
     }
+
+
 }
 
-public class Race {
+class Race {
+    static AtomicLong startRaceTime;
+
     public static void main(String[] args) {
+        final int distance = 1000;
         final ArrayList<Car> cars = new ArrayList<>();
-        cars.add(new Car("Honda", 250)));
+        cars.add(new Car("Honda", 250));
         cars.add(new Car("Lamborgini", 400));
         cars.add(new Car("Ferrari", 350));
         cars.add(new Car("Lanos", 220));
-        final CountDownLatch countDownLatch = new CountDownLatch(cars.size());
-        final ArrayList<RaceCarRunnable>raceCarRunnables=new ArrayList<>();
-        raceCarRunnables.forEach(raceCar->raceCarRunnables.add((new RaceCarRunnable(raceCar, distance, countDownLatch))));
-        final List<Thread> threads = new ArrayList();
+        CountDownLatch countDownLatch = new CountDownLatch(cars.size());
+        List<RaceCarRunnable> raceCarRunnables = new ArrayList<>();
+        cars.forEach(raceCar -> raceCarRunnables.add((new RaceCarRunnable(raceCar, distance, countDownLatch))));
+        List<Thread> threads = new ArrayList();
+        raceCarRunnables.forEach(raceCar -> threads.add(new Thread(raceCar)));
+        startRace(threads);
+
+        try {
+            countDownLatch.await();
+            System.out.println("=================== RACE OVER! ======================");
+            System.out.println("!!!!!!!!!!!!! WINNER: " + defineWinner(raceCarRunnables).getName() + " !!!!!!!!!!!!!!!");
+        } catch (InterruptedException ex) {
+            System.err.print(ex);
+        }
 
     }
+
+    private static RaceCarRunnable defineWinner(final List<RaceCarRunnable> raceCarRunnables) {
+        return raceCarRunnables.stream()
+                .min(Comparator.comparingLong(RaceCarRunnable::getFinishTime))
+                .orElseThrow(NoSuchElementException::new);
+    }
+
 
     static void startRace(List<Thread> cars) {
+        new Thread(() -> {
+            try {
 
+                System.out.println("3...");
+                Thread.sleep(500);
+                System.out.println("2...");
+                Thread.sleep(500);
+                System.out.println("1...");
+                Thread.sleep(500);
+                System.out.println("GO!!!");
+                Thread.sleep(500);
+
+
+                startRaceTime = new AtomicLong(System.currentTimeMillis());
+
+                synchronized (cars) {
+                    for (Thread car : cars) {
+                        car.start();
+                    }
+                }
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        ).start();
     }
 }
+
 
 
